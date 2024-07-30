e27d6627d1ccba7ff14ad0a06f434744990e47cc
Vibhuti Joshi <vp1373@srmist.edu.in>
2024-07-30 11:33:36 +0530

    commit;

---
+++ select/weather_observation_station11.sql
@@ -0,0 +1,22 @@
+SELECT DISTINCT city
+FROM station
+WHERE (city NOT LIKE 'A%' AND
+       city NOT LIKE 'E%' AND
+       city NOT LIKE 'I%' AND
+       city NOT LIKE 'O%' AND
+       city NOT LIKE 'U%' AND
+       city NOT LIKE 'a%' AND
+       city NOT LIKE 'e%' AND
+       city NOT LIKE 'i%' AND
+       city NOT LIKE 'o%' AND
+       city NOT LIKE 'u%')
+   OR (city NOT LIKE '%A' AND
+       city NOT LIKE '%E' AND
+       city NOT LIKE '%I' AND
+       city NOT LIKE '%O' AND
+       city NOT LIKE '%U' AND
+       city NOT LIKE '%a' AND
+       city NOT LIKE '%e' AND
+       city NOT LIKE '%i' AND
+       city NOT LIKE '%o' AND
+       city NOT LIKE '%u');
